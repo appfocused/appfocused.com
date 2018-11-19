a643390bc895dfcbfc02242e1b6384dd3dc37a0b
Vitaly Kondratiev <vitalkond@gmail.com>
2018-11-19 12:03:01 +0000

    relative url for contact form

--- src/components/contact-form/contact-form-service.ts
+++ src/components/contact-form/contact-form-service.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const sendForm = async (model: any) => {
   return axios.post(
-    'https://api.appfocused.com/api/email/send',
+    '/api/email/send',
     { ...model },
     {
       headers: {
