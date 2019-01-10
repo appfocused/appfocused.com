4dc2cc7d442e6f689d1fd7706153b70e600ed5ed
Vitaly Kondratiev <vitalkond@gmail.com>
2019-01-10 20:25:02 +0000

    adds dynamic year in the footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -5,10 +5,13 @@ interface IProps {
   hasScroll: boolean;
 }
 
+const currentYear = new Date().getFullYear();
+
 const Footer = React.memo(({ hasScroll }: IProps) => (
   <footer className={styles.footer}>
     <p>
-      2018 &copy; Appfocused Limited. All rights reserved.<br />
+      2016&mdash;{currentYear} &copy; Appfocused Limited. All rights reserved.
+      <br />
       Appfocused Limited is a company registered in England and Wales with
       company number 10477647
     </p>
